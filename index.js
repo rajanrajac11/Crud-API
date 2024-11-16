4239ead1bcbe7b5ba47e6bb2bd51621c8361a685
Rajan <acharya.rajan.raj11@gmail.com>
2024-11-16 22:19:46 +0545

    nodemon

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world");
+const express = require("express");
+const app = express();
+
+app.listen(3001, () => {
+  console.log("Server is running");
+});
+
+app.get("/", (req, response) => {
+  response.send("Hello there. This");
+});
