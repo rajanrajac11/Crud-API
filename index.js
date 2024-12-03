05af02752eefaf7c6abbb28c6c0f790ecd5acd97
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-03 22:46:21 +0545

    connecting to mongodb

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 
-app.listen(3000, () => {
-  console.log("Server is running at 3000");
-});
-app.get("/", (req, res) => [res.send("Hello everyone Wassup!!!!")]);
+app.get("/", (req, res) => [res.send("Hello everyone Wassup")]);
+
+mongoose
+  .connect(
+    "mongodb+srv://rajanrajac11:BSRw7AYJht6kybwD@cluster0.jbx9n.mongodb.net/Node-API?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => {
+    console.log("Connected!");
+
+    app.listen(3000, () => {
+      console.log("Server is running at 3000");
+    });
+  })
+  .catch(() => {
+    console.log("Connection Failed!");
+  });
