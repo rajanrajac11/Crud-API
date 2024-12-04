19c986b2410001dfe71c4c20458a464886db48a7
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-04 09:43:08 +0545

    creating product

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const Product = require("./models/product.model.js");
 const app = express();
 
-app.use(express.json);
+app.use(express.json());
 
-app.get("/", (req, res) => [res.send("Hello everyone Wassup")]);
+app.get("/", (req, res) => {
+  res.send("Hello everyone Wassup");
+});
 
-app.post("/api/products", (req, res) => {
-  res.send(req.body);
-  console.log(req.body);
+app.post("/api/products", async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.send(500).json({ message: error.message });
+  }
 });
 
 mongoose
