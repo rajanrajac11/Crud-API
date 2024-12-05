a98233290c3f5f4dccb6b6fae6c842ded8ef387c
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-05 22:41:19 +0545

    Property value type fixed in testing

--- Testing/models/product.model.js
+++ Testing/models/product.model.js
@@ -2,21 +2,21 @@ const mongoose = require("mongoose");
 
 const ProductSchema = mongoose.Schema({
   name: {
-    type: "String",
+    type: String,
     required: true,
   },
   quantity: {
-    type: "Number",
+    type: Number,
     required: true,
     default: 0,
   },
   price: {
-    type: "Number",
+    type: Number,
     required: true,
     default: 0,
   },
   image: {
-    type: "String",
+    type: String,
     required: false,
   },
 });
