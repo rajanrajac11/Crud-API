cc016f03d2b2a281689a424e158a7fc7d52567a9
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-09 22:14:03 +0545

    url updated in main index

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.delete("/api/product/:id", async (req, res) => {
 
 mongoose
   .connect(
-    "mongodb+srv://rajanrajac11:eG18N20ocDGWMuq2@api-testing.qfa4j.mongodb.net/?retryWrites=true&w=majority&appName=API-Testing"
+    "mongodb+srv://rajanrajac11:BSRw7AYJht6kybwD@cluster0.jbx9n.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
   )
   .then(() => {
     console.log("Connected!");
