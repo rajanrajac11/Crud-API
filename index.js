020ebc71797a52f0128e333ec9b80f85131b765a
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-10 22:16:55 +0545

    router for api

--- index.js
+++ index.js
@@ -2,69 +2,16 @@ const express = require("express");
 const Product = require("./models/product.model.js");
 const app = express();
 const mongoose = require("mongoose");
+const productRoute = require("./routes/product.route.js");
 
+//middleware
 app.use(express.json());
+
+//routes
+app.use("/api/products", productRoute);
 
 app.get("/", (req, res) => {
   res.send("Wassup Everyone. There is match of Pokhara and Janakpur today.");
-});
-
-app.post("/api/products", async (req, res) => {
-  try {
-    const product = await Product.create(req.body);
-    res.status(200).json(product);
-    console.log(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-app.get("/api/products", async (req, res) => {
-  try {
-    const product = await Product.find({});
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-app.get("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findById(id);
-    res.status(200).json(product);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//update a product
-
-app.put("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndUpdate(id, req.body);
-    if (!product) {
-      return res.status(404).json("Product not found");
-    }
-    const updatedProduct = await Product.findById(id);
-    res.status(200).json(updatedProduct);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
-
-//delete a product
-app.delete("/api/product/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const product = await Product.findByIdAndDelete(id);
-    if (!product) {
-      return res.status(404).json("Product not found");
-    }
-    res.status(200).json("Product Deleted");
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
 });
 
 mongoose
