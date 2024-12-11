9a684ced50331c48d3c377a8586572d3bf25b9ba
Rajan <acharya.rajan.raj11@gmail.com>
2024-12-11 22:18:58 +0545

    Product Controller

---
+++ Testing/controllers/product.controller.js
@@ -0,0 +1,66 @@
+const Product = require("../models/product.model.js");
+
+const getProducts = async (req, res) => {
+  try {
+    const products = await Product.find({});
+    res.status(200).json(products);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const getProduct = async (req, res) => {
+  try {
+    const { id } = req.params();
+    const product = await Product.findById(id);
+    if (!product) {
+      res.status(404).json("Product Not found");
+    } else {
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const addProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const updateProduct = async (req, res) => {
+  try {
+    const { id } = req.params();
+    const product = await Product.findByIdAndUpdate(id);
+    if (!product) {
+      res.status(404).json("Product not updated ");
+    } else {
+      console.log("Product updated");
+      res.status(200).json(product);
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+const deleteProduct = async (req, res) => {
+  try {
+    const { id } = req.params();
+    const product = await Product.findByIdAndDelete(id);
+    res.status(200).json("Product deleted successfully");
+  } catch (error) {
+    res.status(200).json({ message: error.message });
+  }
+};
+
+module.exports = {
+  getProduct,
+  getProducts,
+  addProduct,
+  updateProduct,
+  deleteProduct,
+};
